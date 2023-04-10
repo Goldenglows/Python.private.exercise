b7456253550e9c7a4ed47901b59a115418989ae1
Goldenglows <119770319+Goldenglows@users.noreply.github.com>
2023-04-10 20:49:20 +0800

    Add files via upload

---
+++ 20234102.py
@@ -0,0 +1,21 @@
+scorelist=[86,67,95,100]
+score =  sum(scorelist) / len(scorelist)
+message_dict = {
+    'Name':'Jack',
+    'Age':21,
+    'Folk':'汉',
+    'Score':score
+}
+
+
+scorelist=[86,67,95,100]
+score =  sum(scorelist) / len(scorelist)
+mes_dict = {}
+Name = '李杰'
+Age = 21
+Folk = '汉'
+Score = score
+mes_dict['Name']=Name
+mes_dict['Folk']=Folk
+mes_dict['Age']=Age
+mes_dict['Score']=Score
